9dd2c4189fa22ddbc2c97cb5711a27761d343dfb
Sanjarbuck <orionblankoff@gmail.com>
2021-02-13 14:23:16 +0200

    add if statement for mobile

--- src/index.js
+++ src/index.js
@@ -3,31 +3,38 @@ const $body = document.querySelector("body")
 let imageElement
 let selector
 let isRun = false
+let isMobile = false
 
-$show_img.forEach(el => {
-    el.addEventListener("mouseenter", (ev) => {
-        el.classList.add("show")
-        selector = el
-        imageElement = el.querySelector(".img-hover")
-        isRun = true
-    })
-    el.addEventListener("mouseleave", () => {
-        el.classList.remove("show")
-        setTimeout(()=> {
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    isMobile = true
+}
+
+if (!isMobile) {
+    $show_img.forEach(el => {
+        el.addEventListener("mouseenter", (ev) => {
+            el.classList.add("show")
+            selector = el
+            imageElement = el.querySelector(".img-hover")
+            isRun = true
+            console.log(navigator)
+        })
+        el.addEventListener("mouseleave", () => {
+            el.classList.remove("show")
             isRun = false
-            console.log('off')
-        }, 450)
+        })
     })
-})
 
-$body.addEventListener("mousemove", ev => {
-    if (isRun && imageElement) {
-        const x = ev.clientX - selector.offsetLeft - imageElement.offsetWidth / 2
-        const y = ev.pageY - selector.offsetTop - selector.offsetHeight / 2 - imageElement.offsetHeight / 2
-        imageElement.style.transform = `translate(${x}px, ${y}px)`
-    }
-})
+    $body.addEventListener("mousemove", ev => {
+        if (isRun && imageElement) {
+            const x = ev.clientX - selector.offsetLeft - imageElement.offsetWidth / 2
+            const y = ev.pageY - selector.offsetTop - selector.offsetHeight / 2 - imageElement.offsetHeight / 2
+            imageElement.style.transform = `translate(${x}px, ${y}px)`
+        }
+    })
+}
 
-var lazyLoadInstance = new LazyLoad({
-    threshold: 50
-})
+if (document.querySelector('.lazy')) {
+    const lazyLoadInstance = new LazyLoad({
+        threshold: 30
+    })
+}
